10d6d5bd2779be31d52b4bcaa3779ed4f73e68a0
Samie <samrasolomon40@gmail.com>
2022-06-15 18:28:54 +0000

    Made response for every resolution

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     if (promise) {
-      console.log('Got a response from the API');
       resolve({
         status: 200,
         body: 'success',
@@ -9,5 +8,6 @@ export default function handleResponseFromAPI(promise) {
     } else {
       reject();
     }
+  console.log('Got a response from the API');
   });
 }
