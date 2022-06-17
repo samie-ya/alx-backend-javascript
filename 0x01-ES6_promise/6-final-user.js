ac955c56eb82af4cb37434e7522f53aca2ce7e75
Samie <samrasolomon40@gmail.com>
2022-06-17 23:02:07 +0000

    Added catch method

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,6 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((val) => val);
+    .then((val) => val)
+    .catch((err) => err);
 }
