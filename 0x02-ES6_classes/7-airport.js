98d84077512cefba536868bfc9ccd3d583e17973
Samie <samrasolomon40@gmail.com>
2022-06-17 09:18:20 +0000

    Added a file

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof (name) === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof (code) === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
