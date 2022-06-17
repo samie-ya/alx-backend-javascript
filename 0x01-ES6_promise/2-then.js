d7e38d530ce2a8ba012d950f2fce91d4cb927a40
Samie <samrasolomon40@gmail.com>
2022-06-17 18:15:29 +0000

    Removed return

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -6,7 +6,7 @@ export default function handleResponseFromAPI(promise) {
         body: 'success',
       });
     } else {
-      reject(return new Error());
+      reject(new Error());
     }
     console.log('Got a response from the API');
   });
