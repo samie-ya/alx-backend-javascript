38337f66fc1eea67eab0935d0b6e6c21192c1a10
Samie <samrasolomon40@gmail.com>
2022-06-17 18:14:38 +0000

    Made reject return an error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,13 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
-    resolve({
+    if (promise) {
+      resolve({
         status: 200,
         body: 'success',
       });
-    reject(new Error());
+    } else {
+      reject(return new Error());
+    }
     console.log('Got a response from the API');
   });
 }
