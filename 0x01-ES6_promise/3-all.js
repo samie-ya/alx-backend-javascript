daa17a55f18c5ca3a8cadab23d2bcebd5918012d
Samie <samrasolomon40@gmail.com>
2022-06-17 19:13:21 +0000

    Added catch method

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,10 +4,16 @@ export default function handleProfileSignup() {
   uploadPhoto()
     .then((val) => {
       process.stdout.write(`${val.body} `);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
     });
   createUser()
     .then((val) => {
       process.stdout.write(`${val.firstName} `);
       process.stdout.write(`${val.lastName}\n`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
