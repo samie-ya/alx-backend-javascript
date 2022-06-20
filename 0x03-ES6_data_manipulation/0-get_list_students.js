92694a6d7b016c34970ab89aaab481167e8723d8
Samie <samrasolomon40@gmail.com>
2022-06-20 16:31:52 +0000

    Added a file and README

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const newArray = [];
+  newArray.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  newArray.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  newArray.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+  return newArray;
+}
