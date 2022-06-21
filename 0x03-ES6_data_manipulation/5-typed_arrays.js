f074bb3915084401f636603648bea97a17854fdd
Samie <samrasolomon40@gmail.com>
2022-06-21 13:56:56 +0000

    Added a file

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  const message = 'Position outside range'; // eslint-disable-line no-unused-vars
+  const array = new ArrayBuffer(length);
+  const val = new DataView(array);
+  try {
+    val.setInt8(position, value);
+  } catch (message) {
+    throw new Error(message);
+  }
+  return val;
+}
