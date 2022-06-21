15dda5c39338e63357a17834eb4e4f430aa84376
Samie <samrasolomon40@gmail.com>
2022-06-21 20:15:30 +0000

    Added a file

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  const list = [];
+  if (startString === '') {
+    return '';
+  }
+  for (const subset of set) {
+    if (subset.includes(startString)) {
+      list.push(subset.replace(startString, ''));
+    }
+  }
+  return list.join('-');
+}
