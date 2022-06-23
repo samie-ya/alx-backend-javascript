0ba7d5f241e8bc82799b166a13d9b592c9ec0cc2
samie-ya <80330149+samie-ya@users.noreply.github.com>
2022-06-23 12:28:47 +0300

    Delete 0x00-python_variable_annotations directory

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""This script will add annontation to function argument"""
-
-
-def add(a: float, b: float) -> float:
-    """This function will add float to return"""
-    return a + b
