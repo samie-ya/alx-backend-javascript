26ff687aad3724504eb0d292631e8bd9b347f0e3
Samie <samrasolomon40@gmail.com>
2022-06-26 11:55:30 +0000

    Added then, catch, finally and returned a Promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'success',
-      });
-    } else {
-      reject(new Error());
-    }
-    console.log('Got a response from the API');
-  });
+  const dict = {};
+  dict.status = 200;
+  dict.body = 'success';
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
