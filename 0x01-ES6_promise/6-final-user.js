d7dc5d96a651bd8e4d8456ff364f0031020442d5
Samie <samrasolomon40@gmail.com>
2022-06-26 15:30:00 +0000

    Returned the right array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,5 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((val) => val)
-    .catch((err) => err);
+    .then((value) => value);
 }
