496b12b1810b9de99839515685188176c42d956a
Samie <samrasolomon40@gmail.com>
2022-06-26 15:59:31 +0000

    Modified error result

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((value) => value);
+    .then((value) => {
+      value.forEach((val) => {
+        const result = val;
+        if (result.reason) {
+          const err = result.reason.toString();
+          result.reason = err;
+        }
+      });
+    });
 }
