0af213f4d995be42eed88269172a2e48215525b5
Samie <samrasolomon40@gmail.com>
2022-08-08 20:03:21 +0000

    Used console.log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@
 
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
+  console.log(`Your name is: ${data.toString()}`);
   console.log('This important software is now closing');
   process.exit();
 });
