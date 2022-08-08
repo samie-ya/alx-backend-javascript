ddaef3fc75f236c4f7c7efb89f6ee25d3f0ad1bf
Samie <samrasolomon40@gmail.com>
2022-08-08 16:17:29 +0000

    Added a new_line

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 // This script will display message to STDOUT
 
 function displayMessage(string) {
-  process.stdout.write(string);
+  process.stdout.write(string + '\n');
 }
 
 module.exports = displayMessage;
