f6d09df5597a56b53e8947888b5ffa6847fbd38c
Samie <samrasolomon40@gmail.com>
2022-08-08 20:02:10 +0000

    Used process std-in

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,8 @@
 // This script will take input from command
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  readline.close();
-});
-
-readline.on('close', function ending() {
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
   console.log('This important software is now closing');
-  process.exit(0);
+  process.exit();
 });
