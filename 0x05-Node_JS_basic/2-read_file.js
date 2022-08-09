e8b3d3b02a062fd829368199d8c2e955d9818f38
Samie <samrasolomon40@gmail.com>
2022-08-09 13:31:47 +0000

    Added a missing a space

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 function countStudents(file) {
   if (fs.existsSync(file)) {
-    const content = fs.readFileSync(file, {encoding: 'utf-8'});
+    const content = fs.readFileSync(file, 'utf-8');
     const data = content.split('\n');
     const list = [];
     for (const i of data) {
@@ -20,22 +20,24 @@ function countStudents(file) {
     let counter = 0;
     for (const i of newList) {
       for (let j = 0; j < i.length; j += 1) {
-        if (i[j] === 'CS') {
-          counter += 1;
-          fieldCs += 1;
-          newCs.push(i[0]);
-        }
-        if (i[j] === 'SWE') {
-          counter += 1;
-          fieldSwe += 1;
-          newSwe.push(i[0]);
+        if (i[j] !== '') {
+          if (i[j] === 'CS') {
+            counter += 1;
+            fieldCs += 1;
+            newCs.push(i[0]);
+          }
+          if (i[j] === 'SWE') {
+            counter += 1;
+            fieldSwe += 1;
+            newSwe.push(i[0]);
+          }
         }
       }
     }
 
     console.log(`Number of students: ${counter}`);
     console.log(`Number of students in CS: ${fieldCs}. List: ${newCs.join(', ')}`);
-    console.log(`Number of students in SWE: ${fieldSwe}. List : ${newSwe.join(', ')}`);
+    console.log(`Number of students in SWE: ${fieldSwe}. List: ${newSwe.join(', ')}`);
   } else {
     throw new Error('Cannot load the database');
   }
