f577770acb15ef4edc2a8f3168df61b998a18200
Samie <samrasolomon40@gmail.com>
2022-08-09 16:36:43 +0000

    Added throw error

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -12,43 +12,47 @@ const app = http.createServer((request, response) => {
   response.setHeader('Content-Type', 'text/plain');
   switch (request.url) {
     case '/students':
-      fsPromise.readFile(argv[1])
-        .then((content) => {
-          response.writeHead(200);
-          const str = content.toString();
-          const data = str.split('\n');
-          const list = [];
-          for (const i of data) {
-            const student = i.split(',');
-            list.push(student);
-          }
+      if (fs.existsSync(argv[1])) {
+        fsPromise.readFile(argv[1])
+          .then((content) => {
+            response.writeHead(200);
+            const str = content.toString();
+            const data = str.split('\n');
+            const list = [];
+            for (const i of data) {
+              const student = i.split(',');
+              list.push(student);
+            }
 
-          const newList = list.slice(1, list.length);
-          let fieldCs = 0;
-          const newCs = [];
-          let fieldSwe = 0;
-          const newSwe = [];
-          let counter = 0;
-          for (const i of newList) {
-            for (let j = 0; j < i.length; j += 1) {
-              if (i[j] === 'CS') {
-                counter += 1;
-                fieldCs += 1;
-                newCs.push(i[0]);
-              }
-              if (i[j] === 'SWE') {
-                counter += 1;
-                fieldSwe += 1;
-                newSwe.push(i[0]);
+            const newList = list.slice(1, list.length);
+            let fieldCs = 0;
+            const newCs = [];
+            let fieldSwe = 0;
+            const newSwe = [];
+            let counter = 0;
+            for (const i of newList) {
+              for (let j = 0; j < i.length; j += 1) {
+                if (i[j] === 'CS') {
+                  counter += 1;
+                  fieldCs += 1;
+                  newCs.push(i[0]);
+                }
+                if (i[j] === 'SWE') {
+                  counter += 1;
+                  fieldSwe += 1;
+                  newSwe.push(i[0]);
+                }
               }
             }
-          }
-          const string = 'This is the list of our students\n'
-                         + `Number of students: ${counter}\n`
-                         + `Number of students in CS: ${fieldCs}. List: ${newCs.join(', ')}\n`
-                         + `Number of students in SWE: ${fieldSwe}. List: ${newSwe.join(', ')}`;
-          response.end(string);
-        });
+            const string = 'This is the list of our students\n'
+                           + `Number of students: ${counter}\n`
+                           + `Number of students in CS: ${fieldCs}. List: ${newCs.join(', ')}\n`
+                           + `Number of students in SWE: ${fieldSwe}. List: ${newSwe.join(', ')}`;
+            response.end(string);
+          });
+      } else {
+        throw new Error('Cannot load the database');
+      }
       break;
     default:
       response.writeHead(200);
