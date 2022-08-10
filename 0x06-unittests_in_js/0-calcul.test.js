d108e82eea420bbc43bb7a7e01ad352fb58875d6
Samie <samrasolomon40@gmail.com>
2022-08-10 16:21:23 +0000

    Added strictEqual

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,39 +5,39 @@ const assert = require('assert');
 
 describe('calculateNumber', () => {
   it('...', () => {
-    assert.equal(calculateNumber(1, 0), 1);
+    assert.strictEqual(calculateNumber(1, 0), 1);
   });
   it('...', () => {
-    assert.equal(calculateNumber(1, -1.2), 0);
+    assert.strictEqual(calculateNumber(1, -1.2), 0);
   });
   it('...', () => {
-    assert.equal(calculateNumber(0.99, -1.2), 0);
+    assert.strictEqual(calculateNumber(0.99, -1.2), 0);
   });
   it('...', () => {
-    assert.equal(calculateNumber(1.5, 1.5), 4);
+    assert.strictEqual(calculateNumber(1.5, 1.5), 4);
   });
   it('...', () => {
-    assert.equal(calculateNumber(-9.65, -23.56), -34);
+    assert.strictEqual(calculateNumber(-9.65, -23.56), -34);
   });
   it('...', () => {
-    assert.equal(calculateNumber(76, -1), 75);
+    assert.strictEqual(calculateNumber(76, -1), 75);
   });
   it('...', () => {
-    assert.equal(calculateNumber(345.1, 1.9), 347);
+    assert.strictEqual(calculateNumber(345.1, 1.9), 347);
   });
   it('...', () => {
-    assert.equal(calculateNumber(0, 0), 0);
+    assert.strictEqual(calculateNumber(0, 0), 0);
   });
   it('...', () => {
-    assert.equal(calculateNumber(-100, 100.8), 1);
+    assert.strictEqual(calculateNumber(-100, 100.8), 1);
   });
   it('...', () => {
-    assert.equal(calculateNumber(134567, -1.2), 134566);
+    assert.strictEqual(calculateNumber(134567, -1.2), 134566);
   });
   it('...', () => {
-    assert.equal(calculateNumber(56.0, -9.6), 46);
+    assert.strictEqual(calculateNumber(56.0, -9.6), 46);
   });
   it('...', () => {
-    assert.equal(calculateNumber(0.01, -1.2), -1);
+    assert.strictEqual(calculateNumber(0.01, -1.2), -1);
   });
 });
