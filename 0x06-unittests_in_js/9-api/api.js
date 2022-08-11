6053fef79ad7389bc41cb7b989ce758a86ccafa8
Samie <samrasolomon40@gmail.com>
2022-08-11 16:58:25 +0000

    Added a file

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,19 @@
+// This script will create a server with express
+
+const express = require('express');
+const port = 7865;
+const host = 'localhost';
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (request, response) => {
+  const cart_id = request.params.id;
+  response.send(`Payment methods for cart ${cart_id}`);
+});
+
+app.listen(port, host, () => {
+  process.stdout.write('API available on localhost port 7865');
+});
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,26 @@
+// This script will tst a running server
+
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Cart page', () => {
+  it('Testing response of a request', (done) => {
+    request('http://localhost:7865/cart/12', (error, resp, body) => {
+        expect(resp.statusCode).to.be.equal(200);
+      });
+    done();
+  });
+  it('Testing body of a request', (done) => {
+    request('http://localhost:7865/cart/12', (error, resp, body) => {
+      expect(body).to.be.equal('Payment methods for cart 12');
+    });
+    done();
+  });
+  it('Testing failure of request', (done) => {
+    request('http://localhost:7865/cart/hello', (error, resp, body) => {
+      expect(resp.statusCode).to.be.equal(404);
+    });
+    done();
+  });
+});
