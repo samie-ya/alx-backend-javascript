4869199a7997f24b5ce3fa022d2c5ab150fc54e2
Samie <samrasolomon40@gmail.com>
2022-08-11 13:56:42 +0000

    Modified assertion to chai

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,14 @@
 // This script will test a promise
 
 const getPaymentTokenFromAPI = require('./6-payment_token');
-const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect
 
 describe('Testing a promise', () => {
   it('Testing a promise when success is true', (done) => {
     const resolved = getPaymentTokenFromAPI(true);
-    const given = { data: 'Successful response from the API' };
-    assert(resolved, given);
+    //const given = { data: 'Successful response from the API' };
+    expect(resolved).to.be.a('Promise');
     done();
   });
 });
