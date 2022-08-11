abbdfc8e2d5dc177f24ef96c26391b1f772a83cf
Samie <samrasolomon40@gmail.com>
2022-08-11 06:18:50 +0000

    Added a file

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+// This script will create a spy test
+
+const { Utils } = require('./utils.js');
+const { sendPaymentRequestToApi } = require('./4-payment.js');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const assert = require('assert');
+
+describe('Stubing on calculateNumber', () => {
+  it('I spy with my little eye', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub.withArgs('SUM', 100, 20).returns(10);
+    const spy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(console.log.calledWith('The total is: 10')).to.be.true;
+    stub.restore();
+    spy.restore();
+  });
+});
